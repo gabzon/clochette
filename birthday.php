09ba195f9d5908d82595549f780658881c7e554d
gabzon <gab.zambrano@gmail.com>
2016-02-20 12:35:10 +0100

    voici quelque idess

--- birthday.php
+++ birthday.php
@@ -8,13 +8,13 @@
     <?php get_template_part('templates/page', 'header'); ?>
     <div class="row">
         <div class="col-md-6">
-            <img src="http://localhost/clochette/web/app/uploads/2016/02/anni-1.png" alt="anni-1" class="img-responsive" />
+            <img src="http://bqhdcmfo.preview.infomaniak.com/wp-content/uploads/2016/02/anni-1.png" alt="anni-1" class="img-responsive" />
         </div>
         <div class="col-md-6">
-            <img src="http://localhost/clochette/web/app/uploads/2016/02/anni-2.png" alt="anni-2" class="img-responsive" />
+            <img src="http://bqhdcmfo.preview.infomaniak.com/wp-content/uploads/2016/02/anni-2.png" alt="anni-2" class="img-responsive" />
         </div>
     </div>
-
+<br>
     <div class="jumbotron">
       <h1>THEME DE LA SOIREE</h1>
       <br>
@@ -23,7 +23,6 @@
           Heros ou heroïnes - gentils ou méchants de <br>
           BD - séries tv - films - comédies musicales - jeux vidéos<br>
           <span class="no-thinkerbell">Ah!</span> et.. n'oubliez pas votre déguisement! <span class="no-thinkerbell">(-;</span> <br>
-          (en pièce jointe,  quelque idées idées)<br>
           magnifique cocktail dînatoire grâce à mon ami traiteur <a href="http://www.vidonne.ch/">André Vidonne</a> <br>
           et quelques belles  surprises vous attendent..<br>
           au plaisir de vous voir, on va bien s'amuser!<br>
@@ -31,6 +30,8 @@
           p'tite fée
       </p>
     </div>
-
+    <br>
+    <h2>Voici quelque idées idées</h2>
+    <br>
     <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
