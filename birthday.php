fa89e3a203b958b740a8c6a340eb8668a687c31b
gabzon <gab.zambrano@gmail.com>
2016-02-21 21:30:04 +0100

    gif added

--- birthday.php
+++ birthday.php
@@ -19,9 +19,11 @@
     <div class="ui yellow inverted segment center aligned" style="color:black">
         <h1 style="color:black">THEME DE LA SOIREE</h1>
         <h2 style="color:black" class="thinkerbell">
-            <u><span style="color:green">Heros ou heroïnes</span> - <span style="color:brown">gentils ou méchants</span> des:</u><br>        
+            <u><span style="color:green">Heros ou heroïnes</span> - <span style="color:brown">gentils ou méchants</span> des:</u><br>
             <span style="color:blue">dessins animés - séries tv - films - comédies musicales - opéras - jeux vidéos</span><br>
-            Ah<span class="no-thinkerbell"> !</span> et.. n'oubliez pas votre déguisement<span class="no-thinkerbell"> ! <i class="smile icon"></i></span> <br>
+            Ah<span class="no-thinkerbell"> !</span> et.. n'oubliez pas votre déguisement<span class="no-thinkerbell"> ! </span> <br>
+            <img src="http://bqhdcmfo.preview.infomaniak.com/wp-content/uploads/2016/02/Gros_Smileys_151.gif" alt="Gros_Smileys_151" />
+            <br>
             magnifique cocktail dînatoire grâce à mon ami traiteur André Vidonne<br>
             et quelques belles  surprises vous attendent..<br>
             au plaisir de vous voir, on va bien s'amuser <span class="no-thinkerbell">!</span><br>
