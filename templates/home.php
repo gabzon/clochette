aae15d33f7990a0ba2da0b48dbcc3ed72c7af159
gabzon <gab.zambrano@gmail.com>
2016-07-15 22:47:07 +0200

    version 1 videos uploaded

---
+++ templates/home.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Template Name: home
+*/
+?>
+
+<!-- Header -->
+  <header>
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-12">
+                  <div class="intro-text">
+                      <span class="name">Imagine your music</span>
+                      <hr class="star-light">
+                      <span class="skills">Catchy tunes & all styles</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </header>
+
+<?php get_template_part('templates/portfolio'); ?>
+
+<?php get_template_part('templates/about'); ?>
+
+<?php get_template_part('templates/contact'); ?>
---
+++ templates/portfolio.php
@@ -0,0 +1,66 @@
+<!-- Portfolio Grid Section -->
+<section id="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Video Game Trailers Rescored</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 portfolio-item">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="//player.vimeo.com/video/174723655"></iframe>
+                </div>
+            </div>
+            <div class="col-sm-6 portfolio-item">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="//player.vimeo.com/video/174725993"></iframe>
+                </div>
+            </div>
+        </div>
+        <br><br><br>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Video GamePlay Rescored</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 portfolio-item">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="//player.vimeo.com/video/174684618"></iframe>
+                </div>
+            </div>
+            <div class="col-sm-4 portfolio-item">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="//player.vimeo.com/video/174705992"></iframe>
+                </div>
+            </div>
+            <div class="col-sm-4 portfolio-item">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="//player.vimeo.com/video/174720818"></iframe>
+                </div>
+            </div>
+        </div>
+        <br><br><br>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Video Trailer Movies Rescored</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 portfolio-item">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="//player.vimeo.com/video/174728441"></iframe>
+                </div>
+            </div>
+            <div class="col-sm-6 portfolio-item">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe src="//player.vimeo.com/video/174729662"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
